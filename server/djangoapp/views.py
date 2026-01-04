0c9c8f1448b5bb8fa0754b54624916cdfcc1f8b8
keenant400 <keenant400@gmail.com>
2026-01-04 06:45:52 -0500

    Added Make and Model

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -81,9 +81,10 @@ def registration(request):
         return JsonResponse(data)
 
 def get_cars(request):
-    if CarModel.objects.count() == 0:
+    count = CarMake.objects.filter().count()
+    print(count)
+    if(count == 0):
         initiate()
-
     car_models = CarModel.objects.select_related('car_make')
     cars = []
     for car_model in car_models:
