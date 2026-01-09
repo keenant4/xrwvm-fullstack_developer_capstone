78a923cb5468e3456510aeb0cd119221aecc2c33
keenant4 <keenant400@gmail.com>
2026-01-09 18:52:44 -0500

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -17,11 +17,17 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     "localhost",
-    "https://keenant400-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai",
+    (
+        "https://keenant400-8000.theiadockernext-1-labs-prod-"
+        "theiak8s-4-tor01.proxy.cognitiveclass.ai"
+    ),
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://keenant400-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai",
+    (
+        "https://keenant400-8000.theiadockernext-1-labs-prod-"
+        "theiak8s-4-tor01.proxy.cognitiveclass.ai"
+    ),
 ]
 
 REST_FRAMEWORK = {
